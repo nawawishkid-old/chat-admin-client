b1229b3408bb7d7667e2b20deb9461f8e1f8d473
Nawawish <hydrokidz@gmail.com>
2018-09-16 22:46:25 +0700

    Can accepts multiple API models

--- src/services/api/utils.js
+++ src/services/api/utils.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-export const withApi = name => Component => {
-  const api = require(`~/src/services/api/${name}`).default;
+/**
+ * Attach API model object(s) from given API model name to the given component as a prop.
+ *
+ * @param {String|String[]} name String or Array of string name of API model
+ * @see ~/src/services/api/*
+ * @returns {Function} New React functional component with attached `api` props.
+ */
+export const withApi = (...name) => Component => {
+  console.log("name: ", name);
+  const theNames = name[0].constructor.name === "Array" ? [...name[0]] : name;
+  const api = {};
+
+  theNames.forEach(
+    name => (api[name] = require(`~/src/services/api/${name}`).default)
+  );
 
   return props => <Component api={api} {...props} />;
 };
