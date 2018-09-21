1ce7e2662de0be05f03baa60f5ff011771d97a51
Nawawish <hydrokidz@gmail.com>
2018-09-21 17:47:23 +0700

    Wrote JSDoc

--- src/services/route/Group.js
+++ src/services/route/Group.js
@@ -1,29 +1,48 @@
+// @format
 /**
  * Should rename to RouteGroup
  */
 class RouteGroup {
+  /**
+   * @property {Array} Array of member routes
+   */
   routes = [];
 
+  /**
+   * Create route groups.
+   * @param {String} basePath Base path of this route group.
+   */
   constructor(basePath) {
     // Slice out trailing slash of basepath, to prevent duplicate slash path.
-    basePath =
+    this.basePath =
       basePath[basePath.length - 1] === "/" ? basePath.slice(0, -1) : basePath;
-    this.basePath = basePath;
   }
 
+  /**
+   * Add route specific detail to the group
+   *
+   * @param {String} path Route's path
+   * @param {React.Component} component Component to be displayed for the route
+   * @param {Object} options Additional options of route.
+   */
   add = (path, component, options = {}) =>
     console.log(this.basePath + path, component.constructor.name) ||
     this.routes.push({ path: this.basePath + path, component, options });
 
+  /**
+   * Default page when there is no matched route.
+   *
+   * @param {React.Component} component Component to be displayed.
+   */
   default = component => this.routes.push({ component, options: {} });
 }
 
 /*
-options = {
+routeOptions = {
   exact: Boolean,
   redirect: String,
   auth: Boolean,
-  permission: String
+  permission: String // Future feature, maybe.
 }
 */
 
