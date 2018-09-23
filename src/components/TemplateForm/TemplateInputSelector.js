9e2b7ac92355e40ca596cd10b18a6807951b5811
Nawawish <hydrokidz@gmail.com>
2018-09-24 00:08:08 +0700

    Made sure that supplied data to the component is an array

--- src/components/TemplateForm/TemplateInputSelector.js
+++ src/components/TemplateForm/TemplateInputSelector.js
@@ -11,31 +11,31 @@ const getFieldOptions = initValue => ({
   initialValue: initValue,
   rules: [{ required: true, message: "This field is required" }],
 });
-const HTMLSelect = ({ data, form, initialValue, ...rest }) => (
-  <Form.Item label="Inputs" key="inputs">
-    {form.getFieldDecorator("inputs", getFieldOptions(initialValue))(
-      <Select mode="multiple" placeholder="Inputs" {...rest}>
-        {data.map((item, index) => (
-          <Option value={item._id} key={item._id}>
-            {item.label}
-          </Option>
-        ))}
-      </Select>,
-    )}
-  </Form.Item>
-);
+const HTMLSelect = ({ data, form, initialValue, ...rest }) => {
+  const inputs = Array.isArray(data) ? data : [data];
+
+  return (
+    <Form.Item label="Inputs" key="inputs">
+      {form.getFieldDecorator("inputs", getFieldOptions(initialValue))(
+        <Select mode="multiple" placeholder="Inputs" {...rest}>
+          {inputs.map((item, index) => (
+            <Option value={item._id} key={item._id}>
+              {item.label}
+            </Option>
+          ))}
+        </Select>,
+      )}
+    </Form.Item>
+  );
+};
 
 HTMLSelect.propTypes = {
   data: PropTypes.any.isRequired,
   form: PropTypes.object.isRequired,
-  initialValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  // initialValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 const LoadableHTMLSelect = loadable(HTMLSelect);
-
-LoadableHTMLSelect.propTypes = {
-  load: PropTypes.func.isRequired,
-};
 
 // Load all template inputs
 const handleHTMLSelectLoad = load => {
