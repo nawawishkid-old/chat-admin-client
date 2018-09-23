e9399bedf8e9569ee1b3cda28deb306ff7d3efb5
Nawawish <hydrokidz@gmail.com>
2018-09-23 16:57:03 +0700

    Added routes for /admin/template-inputs

--- src/data/routes/admin.js
+++ src/data/routes/admin.js
@@ -3,28 +3,51 @@ import { RouteGroup } from "~/src/services/route";
 import {
   PageTemplateAll,
   PageTemplateNew,
-  PageTemplateEdit
+  PageTemplateEdit,
 } from "~/src/pages/Template";
+import {
+  PageTemplateInputAll,
+  PageTemplateInputNew,
+  PageTemplateInputEdit,
+} from "~/src/pages/TemplateInput";
+
 import { Page404, PageAdmin } from "../../pages";
 import { Redirect } from "react-router-dom";
 
 const adminRouter = new RouteGroup("/admin");
 const options = {
   auth: true,
-  permission: "write"
+  permission: "write",
+};
+const optionsForNew = {
+  ...options,
+  exact: true,
+};
+const optionsPermRead = {
+  ...optionsForNew,
+  permission: "read",
 };
 
-adminRouter.add('/', () => <Redirect to="/admin/templates" />, { ...options, exact: true });
-adminRouter.add("/templates", PageTemplateAll, {
-  ...options,
-  permission: "read",
-  exact: true
-});
-adminRouter.add("/templates/new", PageTemplateNew, {
-  ...options,
-  exact: true
-});
-adminRouter.add("/templates/:templateId([a-f0-9]{24})/edit", PageTemplateEdit, options);
+adminRouter.add("/", () => <Redirect to="/admin/templates" />, optionsForNew);
+
+// Template
+adminRouter.add("/templates", PageTemplateAll, optionsPermRead);
+adminRouter.add("/templates/new", PageTemplateNew, optionsForNew);
+adminRouter.add(
+  "/templates/:templateId([a-f0-9]{24})/edit",
+  PageTemplateEdit,
+  options,
+);
+
+// TemplateInput
+adminRouter.add("/template-inputs", PageTemplateInputAll, optionsPermRead);
+adminRouter.add("/template-inputs/new", PageTemplateInputNew, optionsForNew);
+adminRouter.add(
+  "/template-inputs/:templateInputId([a-f0-9]{24})/edit",
+  PageTemplateInputEdit,
+  options,
+);
+
 // Could not set 404 page for /admin route group
 // If set, the <Router> always switch to 404 page.
 // If not, <Router> always display /admin page (PageAdmin)
