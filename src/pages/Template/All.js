0ead81566f10564ec4e3d4ec69f45804789aadd4
Nawawish <hydrokidz@gmail.com>
2018-09-23 07:04:33 +0700

    Unimporant update

--- src/pages/Template/All.js
+++ src/pages/Template/All.js
@@ -4,13 +4,11 @@ import loadable from "~/src/components/Loadable";
 import TemplateFormQuery from "~/src/components/TemplateForm/Query";
 
 const TemplateFormQueryLoadable = loadable(({ data }) =>
-  data.map(doc => (
-    <TemplateFormQuery fieldSchemes={doc.inputs} templateId={doc._id} />
+  data.map((doc, index) => (
+    <TemplateFormQuery key={index} fieldSchemes={doc.inputs} templateId={doc._id} />
   ))
 );
 
-const Wait = () => <h3>Loading...</h3>;
-const Timeout = () => <h3>Timeout!</h3>;
 const handleLoad = load =>
   templateApi.get("get").call((err, data, status) => {
     console.log("STATUS: ", status);
@@ -19,14 +17,11 @@ const handleLoad = load =>
       return;
     }
     console.log("data: ", data);
-    load(data.doc);
+    load(data.data.doc);
   });
 const AllTemplateFormQueries = () => (
   <TemplateFormQueryLoadable
     handleLoad={handleLoad}
-    limit={5000}
-    wait={Wait}
-    timeout={Timeout}
   />
 );
 
