44b8ac68dbdadaa75e567629c19cb1ce424cd367
Nawawish <hydrokidz@gmail.com>
2018-09-24 00:03:50 +0700

    Added 'makeAntdFieldDecorator' function.

--- src/services/form.js
+++ src/services/form.js
@@ -52,7 +52,7 @@ class FormBuilder {
     if (type === "select" && Array.isArray(options)) {
       const { Option } = Select;
       children = options.map((optionScheme, index) => (
-        <Select.Option value={optionScheme._id}>
+        <Select.Option value={optionScheme._id} key={optionScheme._id}>
           {optionScheme.label}
         </Select.Option>
       ));
@@ -96,8 +96,60 @@ class FormBuilder {
 
 const makePasswordInput = () => props => <Input type="password" {...props} />;
 
-const form = { withForm, FormBuilder };
+/**
+ * Create `antd` filed decorator from specific data structure
+ *
+ * @param {Object} scheme
+ * @param {String} scheme.id ID of template form's filed
+ * @param {Object} scheme.options Optional configuration of the field, uses for `antd` filed decorator
+ * @param {Object} scheme.component Object contains data for generating React.Component
+ * @returns {Object} `antd` field's decorator object
+ */
+const makeAntdFieldDecorator = scheme => {
+  const { name, componentScheme } = scheme;
+  const props = componentScheme.hasOwnProperty("props")
+    ? componentScheme.props
+    : {};
+  const decorator = {
+    id: name,
+    options: scheme.options || {},
+  };
+  let selectOptions = null,
+    TheInput;
 
-export { withForm, FormBuilder, form };
+  switch (componentScheme.type) {
+    case "number":
+      TheInput = InputNumber;
+      break;
+    case "select":
+      TheInput = Select;
+      break;
+    default:
+      TheInput = Input;
+  }
+
+  if (Array.isArray(componentScheme.options)) {
+    selectOptions = componentScheme.options.map((item, index) => {
+      return (
+        <Select.Option value={item.value} key={index}>
+          {item.name}
+        </Select.Option>
+      );
+    });
+  }
+
+  if (props.defaultValue !== undefined) {
+    decorator.options.initialValue = props.defaultValue;
+    delete props.defaultValue;
+  }
+
+  decorator.component = <TheInput {...props}>{selectOptions}</TheInput>;
+
+  return decorator;
+};
+
+const form = { withForm, FormBuilder, makeAntdFieldDecorator };
+
+export { withForm, FormBuilder, form, makeAntdFieldDecorator };
 
 export default form;
