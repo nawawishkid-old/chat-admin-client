7b7e40577850a8049be21d6349af009500a23074
Nawawish <hydrokidz@gmail.com>
2018-09-23 16:56:32 +0700

    Deleted unrelated files inherited from master branch.

--- src/components/TemplateBuilder/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Form } from "antd";
-import TemplateBuilder from "./index";
-
-const WithForm = Form.create()(TemplateBuilder);
-
-storiesOf("TemplageBuilder", module).add("default", () => <WithForm />);
