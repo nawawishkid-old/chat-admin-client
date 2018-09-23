17471e799669b2e685d221c39c320411fb75cba6
Nawawish <hydrokidz@gmail.com>
2018-09-23 07:05:43 +0700

    Comment out the  scheme 'cause I have to create it manually

--- src/data/form-schemes/template-form/builder.js
+++ src/data/form-schemes/template-form/builder.js
@@ -56,30 +56,31 @@ export const closingTagFieldScheme = {
   }
 };
 
-export const inputsFieldScheme = {
-  label: "Inputs",
-  name: "inputs",
-  options: {
-    rules: [required]
-  },
-  componentScheme: {
-    type: "select",
-    props: {
-      placeholder: "Inputs"
-    },
-    options: [
-      { text: "Choose me!", value: "1" },
-      { text: "Choose me!", value: "2" }
-    ]
-  }
-};
+// export const inputsFieldScheme = {
+//   label: "Inputs",
+//   name: "inputs",
+//   options: {
+//     rules: [required]
+//   },
+//   componentScheme: {
+//     type: "select",
+//     props: {
+// 			placeholder: "Inputs",
+// 			mode: 'multiple' // Allow to select multiple item
+//     },
+//     options: [
+//       { label: "Choose me1!", _id: "1" }, // label = option.textContent, _id = option.value
+//       { label: "Choose me2!", _id: "2" }
+//     ]
+//   }
+// };
 
 const fieldSchemes = [
   nameFieldScheme,
   contentFieldScheme,
   openTagFieldScheme,
   closingTagFieldScheme,
-  inputsFieldScheme
+  // inputsFieldScheme
 ];
 
 const templateFormBuilderScheme = {
