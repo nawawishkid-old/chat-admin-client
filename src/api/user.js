c99ce1c020b01c96b433a896616f53e58c5ee5ba
Nawawish <hydrokidz@gmail.com>
2018-09-25 22:35:53 +0700

    Added user API route

---
+++ src/api/user.js
@@ -0,0 +1,22 @@
+import APIGroup from "~/src/services/api/Model";
+
+const userApi = new APIGroup("/api/users");
+const options = { auth: true };
+
+userApi.add("get", "get", "/", options);
+userApi.add("create", "post", "/", options);
+userApi.add("update", "post", "/update", options);
+userApi.add("delete", "post", "/delete", options);
+
+userApi.on("all", (err, res, status) => {
+  if (err) {
+    console.error(`${status}: ${err.msg}`);
+    return;
+  }
+
+  console.log(`${status}: ${res.msg}`);
+});
+
+export { userApi };
+
+export default userApi;
