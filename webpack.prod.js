a857a3e923a158d1bf5750940b9883429d82507d
Nawawish Samerpark <hydrokidz@gmail.com>
2018-11-10 15:06:14 +0700

    updated: use dotenv to handle env vars and webpack-pwa-manifest to generate manifest.json

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,7 @@
 const merge = require("webpack-merge");
-const webpack = require("webpack");
 const common = require("./webpack.common.js");
-// const env = {
-//   WEBPACK_APP_BASE_PATH: "/chat-admin/"
-// };
 
 module.exports = merge(common, {
   mode: "production",
-	output: {
-		publicPath: '/'
-	},
-  // output: {
-  //   publicPath: env.WEBPACK_APP_BASE_PATH
-  // },
   devtool: "cheap-module-source-map",
-  // plugins: [new webpack.DefinePlugin(env)]
 });
